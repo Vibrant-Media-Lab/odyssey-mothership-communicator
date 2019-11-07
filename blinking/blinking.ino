722dbb01f9385278d9afa336eda90c31fae4a14d
asaba96 <asaba96@pitt.edu>
2019-11-06 19:25:44 -0500

    misc. additions

--- blinking/blinking.ino
+++ blinking/blinking.ino
@@ -5,11 +5,25 @@
     @Author: Andrew Saba
 */
 
+#include "pitches.h"
+
 const int LED_1 = A1;
 const int LED_2 = A2;
 const int LED_3 = A3;
+const int SPEAKER_PIN = 8;
 
-const int LOOP_DELAY = 100; // in ms, so 50hz
+const int TONE_1 = NOTE_B0;
+const int TONE_2 = NOTE_G5;
+const int TONE_3 = NOTE_DS8;
+
+const int NOTE_DURATION = 4;
+
+const int LOOP_DELAY = 100; // in ms, so 10hz
+const int HEART_DELAY = 1000; // in ms
+
+// min and max time on
+const int MIN_TIME =  3;
+const int MAX_TIME = 7;
 
 void setup() {
     // initialize LED pins as outputs
@@ -30,6 +44,7 @@ void display_error(){
         digitalWrite(LED_1, HIGH);
         digitalWrite(LED_2, HIGH);
         digitalWrite(LED_3, HIGH);
+        digitalWrite(LED_BUILTIN, HIGH);
         Serial.println("SOMETHING HAPPENED");
         delay(100000);
     }
@@ -37,13 +52,26 @@ void display_error(){
 
 void loop() {
     long curr_led = random(3); // inclusive, so must be 3
-    unsigned long on_time = millis() + 1000 * random(3, 7); // between 3 and 7 seconds, in ms
+    unsigned long on_time = millis() + 1000 * random(MIN_TIME, MAX_TIME+1); // inclusive, so add 1
+
+    int heart_state = HIGH;
+    unsigned long heart_time = millis() + HEART_DELAY;
+    digitalWrite(LED_BUILTIN, heart_state);
+
+    unsigned long tone_start = millis() + 1000/NOTE_DURATION * 1.30;
 
     while (true) {
+        // switch heartbeat LED
+        if (millis() >= heart_time) {
+            heart_state = (heart_state == HIGH) ? LOW : HIGH;
+            digitalWrite(LED_BUILTIN, heart_state);
+            heart_time = millis() + HEART_DELAY;
+        }
+
         if (millis() >= on_time) {
             // time to reset
             long new_led = random(2); // inclusive, so must be 3
-            on_time = millis() + 1000 * random(3, 7); // between 3 and 7 seconds
+            on_time = millis() + 1000 * random(MIN_TIME, MAX_TIME+1); // inclusive, so add 1
 
             switch (curr_led){
                 case 0:
--- driver/driver.ino
+++ driver/driver.ino
@@ -19,17 +19,17 @@
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+  NOTE_B0, NOTE_DS8
 };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
+  4, 4
 };
 
 void setup() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  for (int thisNote = 0; thisNote < 2; thisNote++) {
 
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
@@ -48,5 +48,3 @@ void setup() {
 void loop() {
   // no need to repeat the melody.
 }
-
-
